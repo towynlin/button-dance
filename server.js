6efb363db752ef7750a1b4e9453f5762c26fa834
Zachary Crockett <towynlin@gmail.com>
2015-07-14 20:52:50 -0700

    Listen on PORT environment variable for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 var routes = require('./app/routes');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: process.env.PORT || 3000 });
 server.route(routes);
 server.start(function startServer() {
 	console.log('Server running at:', server.info.uri);
